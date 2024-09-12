2fb998b81881917aef60631cd7fdf044ee2a5800
Ackrilll <skawlsgus2@naver.com>
2024-09-12 22:53:53 +0900

    [INIT]

---
+++ src/main/java/com/sparta/newsfeedalone/NewsfeedAloneApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.newsfeedalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsfeedAloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsfeedAloneApplication.class, args);
+    }
+
+}
